3b3c429e17d9d8bcbbc4e10e443a44a77a39696d
Nikolay V <nv-mail@mail.ru>
2022-05-08 13:49:32 +0300

    final versions (+extended) of solution for exercise Module 5 Part 1 Level 1

--- Mod5/hw_M5P1L1version_full.py
+++ Mod5/hw_M5P1L1version_full.py
@@ -3,16 +3,24 @@ import json
 class StringVar():
 	s = 'start again ! '
 
+#	def __init__(self, s1):
+#		if type(s1) == str : 
+#			print(' good start, this variable is string ! ')
+#			self.s = s1
+#		else:
+#			print(' this variable is supposed to be of type string. try it again ')
+#			self.s = ' 42 '
+
 	def fprint(self):
 		print(' ', self.s)
 		pass
 
 	def set(self, s1):
 		if type(s1) == str : 
-			print(' good start, input-variable is string ! ')
+			print(' good start, this variable is string ! ')
 			self.s = s1
 		else:
-			print(' input-variable is supposed to be of type string. try it again ')
+			print(' this variable is supposed to be of type string. try it again ')
 #			self.s = ' 42 '
 		pass
 
@@ -30,7 +38,8 @@ a = StringVar()
 #word2 = 42						#2
 #word2 = [9, 1, 3]				#3
 #word2 = (7, 3 ,6)				#4
-#word2 = {"startlogin": "brunoym123", "person1": "sport123", "person2": "nosport123"}
+#word2 = {9, ' flist '}			#5
+word2 = {"startlogin": "brunoym123", "nikolay": "sport123", "alla": "nosport123"}
 
 #word2 = [[9, 70, 20, 67, 33], 	#6
 #    [60, 20, 94, 14,77],
@@ -38,16 +47,20 @@ a = StringVar()
 #    [39, 47, 25, 97,69],
 #    [83, 13, 100, 1, 85]]
 
-#with open('data511_type5.json', 'w') as f:
-#	json.dump(word2, f)
+#if word == '42' : word2 = 42
+#else: word2 = word
+
+with open('data511_type5.json', 'w') as f:
+	json.dump(word2, f)
 
 with open('data511_type5.json', 'r') as f:
 	word1 = json.load(f)
 
+#a = StringVar(word1)
 a.set(word1)
 
 #a.fprint()
 print(' print as an attribute of StrinVar class (not via methods) : ', a.s)
 
-s2 = a.get()
-print(' string from class StringVar (via get - method) = ', s2)
+s = a.get()
+print(' string from class StringVar (via get - method) = ', s)
